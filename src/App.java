8f84ce0e980ca95ed755934c61cbd42962085bfa
Lgp007-IES <lgueper960@g.educaand.es>
2025-10-23 13:21:00 +0200

    v.0.2 tercer commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, soy Luis!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor introduce tu nombre");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Hola " + nombre);
     }
 }
